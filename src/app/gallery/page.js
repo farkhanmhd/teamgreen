d207cd3b18390e311ba138c4d1a8accb0a9bf2bb
farkhanmhd <farkhanmuhammadv3@gmail.com>
2024-06-16 01:03:52 +0700

    Added gallery page

---
+++ src/app/gallery/page.js
@@ -0,0 +1,43 @@
+import GalleryImage from '../ui/components/GalleryImage';
+
+const GalleryPage = () => {
+  const galleryData = [
+    {
+      image: '/images/gallery-1.jpg',
+      caption:
+        'Preserving the Pristine: Our Digital Actions for a Greener Tomorrow.',
+    },
+    {
+      image: '/images/gallery-2.jpg',
+      caption:
+        'Connected for Change: Harnessing Technology for Environmental Good.',
+    },
+    {
+      image: '/images/gallery-3.jpg',
+      caption: 'Turning Waste into Worth: The Cycle of Renewal Begins with Us.',
+    },
+    {
+      image: '/images/gallery-4.jpg',
+      caption:
+        'Oases in the Concrete Jungle: Cultivating Green Spaces in Urban Hearts.',
+    },
+    {
+      image: '/images/gallery-5.jpg',
+      caption: 'Energizing the Future: Clean Power for a Sustainable World.',
+    },
+  ];
+  return (
+    <main className="h-[100dvh] bg-white px-[30px] pb-[50px] pt-[70px] lg:px-[50px] xl:px-[70px]">
+      <section className="h-full">
+        <h1 className="mb-5 h-[5%] text-sm font-medium">Gallery</h1>
+        <ul className="flex h-[95%] flex-col overflow-hidden xl:flex-row">
+          {galleryData.map((image, index) => (
+            <GalleryImage key={index} {...image} />
+          ))}
+        </ul>
+      </section>
+    </main>
+  );
+};
+
+export default GalleryPage;
---
+++ src/app/ui/components/GalleryImage.js
@@ -0,0 +1,39 @@
+'use client';
+
+import { useAtomValue } from 'jotai';
+import { firstLoadAtom } from '@/app/states/state';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import Image from 'next/image';
+import { isMobile } from '@/app/utils/utils';
+
+const GalleryImage = ({ image, caption }) => {
+  const isFirstLoad = useAtomValue(firstLoadAtom);
+  useGSAP(() => {
+    gsap.to('.gallery-image', {
+      opacity: 1,
+      stagger: 0.1,
+      delay: isFirstLoad && 3.5,
+    });
+  });
+  return (
+    <li className="gallery-image group h-[20%] flex-[1] overflow-hidden opacity-0 duration-300 hover:flex-[3] xl:h-full xl:max-h-full">
+      <figure className="relative h-full">
+        <Image
+          src={image}
+          alt="gallery-image"
+          width={1000}
+          height={1000}
+          className="absolute left-0 top-0 h-full w-full object-cover grayscale duration-300 group-hover:grayscale-0"
+        />
+        <figcaption className="absolute h-full w-full bg-black/50 opacity-0 duration-300 group-hover:opacity-100 group-hover:delay-300">
+          <p className="flex h-full w-full items-center justify-center text-center font-medium text-white delay-300 duration-300 lg:text-sm">
+            {caption}
+          </p>
+        </figcaption>
+      </figure>
+    </li>
+  );
+};
+
+export default GalleryImage;
